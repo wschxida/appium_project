1714d119df2f013101a766a83f332e1ebdef5848
DELL <wschxida@gmail.com>
2021-04-23 10:05:35 +0800

    2.0 Facebook appium

--- service/model/facebook.py
+++ service/model/facebook.py
@@ -1,4 +1,5 @@
 from appium import webdriver
+from selenium import webdriver as sele_webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -6,7 +7,7 @@ import time
 
 
 def start_appium():
-    # 雷电模拟器
+    # # 雷电模拟器
     # desired_caps = {
     #     'platformName': 'Android',
     #     'deviceName': 'emulator-5554',
@@ -22,70 +23,77 @@ def start_appium():
     #     # 直接指定浏览器名称参数为chrome【重点添加了这一步】
     #     # 'browserName': 'chrome',
     #     # 使用指定的浏览器驱动-匹配手机上的谷歌浏览器
-    #     # 'chromedriverExecutable': r'E:/python_project/appium_project/service/model/chromedriver_dir/chromedriver_89.exe'
-    # }
-
-    # 夜神
-    # desired_caps = {
-    #     'platformName': 'Android',
-    #     'deviceName': 'SM_G977N',
-    #     'appPackage': 'com.android.chrome',
-    #     'appActivity': 'com.google.android.apps.chrome.Main',
-    #     'platformVersion': '7',
-    #     'newCommandTimeout': 6000,
-    #     'noReset': True,
-    #     'unicodeKeyboard': True,
-    #     'autoGrantPermissions': True,
-    #     'resetKeyboard': True,
-    #     'fullReset': False,
-    #     # 直接指定浏览器名称参数为chrome【重点添加了这一步】
-    #     # 'browserName': 'chrome',
-    #     # 使用指定的浏览器驱动-匹配手机上的谷歌浏览器
     #     'chromedriverExecutable': r'E:/python_project/appium_project/service/model/chromedriver_dir/chromedriver_89.exe'
     # }
 
-    # 手机
+    # 夜神
     desired_caps = {
         'platformName': 'Android',
-        'deviceName': 'Redmi_Note_7',
+        'deviceName': 'SM_G977N',
         'appPackage': 'com.android.chrome',
         'appActivity': 'com.google.android.apps.chrome.Main',
-        # 'appPackage': 'com.facebook.katana',
-        # 'appActivity': '.activity.FbMainTabActivity',
-        'platformVersion': '10',
+        'platformVersion': '7',
         'newCommandTimeout': 6000,
         'noReset': True,
         'unicodeKeyboard': True,
         'autoGrantPermissions': True,
         'resetKeyboard': True,
         'fullReset': False,
-        'chromedriverExecutable': r'E:/python_project/page_agent_queue_service/cloud_service/chromedriver_dir/chromedriver_81.exe'
+        # 直接指定浏览器名称参数为chrome【重点添加了这一步】
+        # 'browserName': 'Chrome',
+        # 使用指定的浏览器驱动-匹配手机上的谷歌浏览器
+        'chromedriverExecutable': r'E:/python_project/appium_project/service/model/chromedriver_dir/chromedriver_90.exe'
     }
+
+    # # 手机
+    # desired_caps = {
+    #     'platformName': 'Android',
+    #     'deviceName': 'Redmi_Note_7',
+    #     'appPackage': 'com.android.chrome',
+    #     'appActivity': 'com.google.android.apps.chrome.Main',
+    #     # 'appPackage': 'com.facebook.katana',
+    #     # 'appActivity': '.activity.FbMainTabActivity',
+    #     'platformVersion': '10',
+    #     'newCommandTimeout': 6000,
+    #     'noReset': True,
+    #     'unicodeKeyboard': True,
+    #     'autoGrantPermissions': True,
+    #     'resetKeyboard': True,
+    #     'fullReset': False,
+    #     'chromedriverExecutable': r'E:/python_project/page_agent_queue_service/cloud_service/chromedriver_dir/chromedriver_81.exe'
+    # }
 
     driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
     driver.implicitly_wait(10)
 
-    driver.get("http://facebook.com/")
-    time.sleep(2)
-    # # driver.get("http://m.facebook.com/?soft=search")
+    driver.get("https://www.facebook.com/bbc")
+    # driver.get("http://m.sohu.com/")
+    time.sleep(5)
     # # 输入字段
-    driver.find_element_by_xpath('.//*[@text="Search Search"]').click()
-    time.sleep(1)
-    driver.find_element_by_xpath('.//*[@resource-id="main-search-input"]').send_keys("https://www.facebook.com/guosha.san")
-    driver.press_keycode(66)  # 回车
-    time.sleep(3)
+    # driver.find_element_by_xpath('.//*[@text="Search Search"]').click()
+    # time.sleep(1)
+    # driver.find_element_by_xpath('.//*[@resource-id="main-search-input"]').send_keys("https://www.facebook.com/guosha.san")
+    # driver.press_keycode(66)  # 回车
+    # time.sleep(3)
+    #
+    # # 屏幕宽
+    # width = driver.get_window_size()['width']
+    # # 屏幕高
+    # height = driver.get_window_size()['height']
+    #
+    # print(width, height)
+    # x1 = int(0.37 * width)
+    # y1 = int(0.4 * height)
+    # print(x1, y1)
+    # driver.tap([(x1, y1)], 0)
+    # time.sleep(5)
 
     # 屏幕宽
     width = driver.get_window_size()['width']
     # 屏幕高
     height = driver.get_window_size()['height']
-
-    print(width, height)
-    x1 = int(0.37 * width)
-    y1 = int(0.4 * height)
-    print(x1, y1)
-    driver.tap([(x1, y1)], 0)
-    time.sleep(5)
+    driver.swipe(width * 0.5, height * 0.9, width * 0.5, height * 0.1, 1000)
+    WebDriverWait(driver, 2)
 
     contexts = driver.contexts
     print(contexts)
--- service/model/test.py
+++ service/model/test.py
@@ -7,8 +7,11 @@
 
 from selenium import webdriver
 options = webdriver.ChromeOptions()
-options.add_experimental_option('androidPackage', 'com.android.chrome')
-# options.add_argument(r'user-data-dir=E:\python_project\appium_project\service\model\chrome\user_data_1')
-driver = webdriver.Chrome('./chromedriver_dir/chromedriver_89.exe', options=options)
+# options.add_experimental_option('androidPackage', 'com.android.chrome')
+driver = webdriver.Chrome('./chromedriver_dir/chromedriver_90.exe', options=options)
 driver.get('https://m.facebook.com/benger.won')
+
+page_html = driver.page_source
+print(page_html)
+
 driver.quit()
